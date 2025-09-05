3a566a0c1f2d0e37a0d0f1b4b0b280309c40700c
Robert <rbrt@hey.com>
2025-09-05 10:32:36 -0400

    updates to chat page

--- fivetran-ai-experiment/app/chat/page.tsx
+++ fivetran-ai-experiment/app/chat/page.tsx
@@ -16,23 +16,25 @@ export default function Page() {
 
   return (
     <div className="flex flex-col w-full max-w-150 mx-auto h-full justify-between">
-      {messages.map((message) => (
-        <motion.div key={message.id}>
-          <div>{message.role === 'user' ? 'User: ' : 'AI: '}</div>
-          <div>
-            {message.parts.map((part, index) => {
-              if (part.type === 'text') {
-                return <span key={index}>{part.text}</span>;
-              }
-              return null;
-            })}
-          </div>
-        </motion.div>
-      ))}
-
+      <div>
+        {messages.map((message) => (
+          <motion.div key={message.id}>
+            <div>{message.role === 'user' ? 'User: ' : 'AI: '}</div>
+            <div
+              className={message.role === 'user' ? 'text-right' : 'text-left'}>
+              {message.parts.map((part, index) => {
+                if (part.type === 'text') {
+                  return <motion.span key={index}>{part.text}</motion.span>;
+                }
+                return null;
+              })}
+            </div>
+          </motion.div>
+        ))}
+      </div>
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col gap-2 bg-white w-full p-4 rounded-md shadow-lg h-50 justify-between items-start">
+        className="flex flex-col gap-2 bg-white w-full p-4 rounded-md shadow-lg h-40 justify-between items-start">
         <textarea
           className="w-full h-full"
           value={input}
