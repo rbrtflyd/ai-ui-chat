59837c9c56eca8c320febd418a8fa248632095e9
Robert <rbrt@hey.com>
2025-09-09 15:28:42 -0600

    styling updates

--- fivetran-ai-experiment/app/page.tsx
+++ fivetran-ai-experiment/app/page.tsx
@@ -3,7 +3,10 @@ import ChatThreadWrapper from '@/components/Chat/ChatThreadWrapper';
 export default function Home() {
   return (
     <div className="flex flex-col w-full h-full">
-      <div className="w-full h-80 bg-gray-05"></div>
+      <div className="w-full h-80 bg-gray-05 flex items-center justify-center text-3xl">
+        {' '}
+        Dashboard goes here{' '}
+      </div>
       <div className="flex flex-row gap-9 px-8 py-6 justify-center">
         <div className="size-80 bg-white border border-gray-10 rounded-lg" />
         <div className="size-80 bg-white border border-gray-10 rounded-lg" />
